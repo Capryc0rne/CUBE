184e5467b6fa566b104e4adf008aff96dc83f87e
Arthur Crahé <crahe.arthur@gmail.com>
2024-11-16 12:34:35 +0100

    show nextjs logs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -26,7 +26,7 @@ const nextConfig = {
        reactStrictMode: true, // Enable React strict mode for improved error handling
        swcMinify: true,      // Enable SWC minification for improved performance
        compiler: {
-              removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+              removeConsole: false
        },
 }
 
